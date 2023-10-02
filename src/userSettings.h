89d4792a9595bea481cc5d513e7e5daf8ba4827c
pinballash <145190941+pinballash@users.noreply.github.com>
2023-10-02 17:52:16 +0100

    Test commit to make sure its working

--- src/userSettings.h
+++ src/userSettings.h
@@ -1,5 +1,6 @@
 /*
 * wireless networking settings
+* Changes made 02/10/2023
 */
 const char* host = "ESPPOS"; //this will be the hostname on your network, but will be accessible as what you set + .local eg; yourPinballName.local
 const char* ssid = "PinballWifi"; //This is the SSID of the wireless network in range of the pinball machine.
